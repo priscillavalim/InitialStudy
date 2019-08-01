340588d3a65c7f7bfe3779ac96df167c01f13c1e
Priscilla Valim <priscilla.valim@outlook.com>
2019-07-31 23:11:09 -0300

    Estudo de condicional.

---
+++ Catisyou/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Catisyou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you prefer hamburger, french fries or pizza?");
+
+            String answer = Console.ReadLine();
+
+            if (answer == "hamburger")
+            {
+                Console.WriteLine("Black cat");
+            }
+            else if (answer == "french fries")
+            {
+                Console.WriteLine("Tiger cat");
+            }
+            else if (answer == "pizza")
+            {
+                Console.WriteLine("Three colored cat");
+            }
+
+        }
+    }
+}
