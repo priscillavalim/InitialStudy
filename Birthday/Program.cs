20fa11b162557495b889b1ed31cf240e58dcd3f2
Priscilla Valim <priscilla.valim@outlook.com>
2019-07-31 23:37:00 -0300

    Calculando data

---
+++ Birthday/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("When is your birthday?");
+
+            string answer = Console.ReadLine();
+            DateTime birthday = DateTime.Parse(answer);
+
+            if (birthday == DateTime.Today)
+            {
+                Console.WriteLine("Happy Birthday!");
+            }
+            else
+            {
+                int days = (DateTime.Today - birthday).Days;
+                Console.WriteLine($"Days until your birthday:{days}");
+            }
+        }
+    }
+}
